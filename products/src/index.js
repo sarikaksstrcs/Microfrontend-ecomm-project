bf5ba9bdf292c2ae185e47f4fff253bf33b96c4b
Sarika K S <sarika.ks.official@gmail.com>
2026-01-04 11:35:33 +0530

    Add webpack

---
+++ products/src/index.js
@@ -0,0 +1,14 @@
+import faker from 'faker';
+
+let products = '';
+
+for (let i = 0; i < 10; i++) {
+  products += `<div>
+    <h2>${faker.commerce.productName()}</h2>
+    <p>${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()} ${faker.commerce.product()}</p>
+    <strong>$${faker.commerce.price()}</strong>
+  </div>`;
+}
+console.log(products);
+
+export default products;
